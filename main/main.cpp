33299fd077d743e1e819bbe6345eaa37bedf189e
Deck-Wang416 <sc212yw@leeds.ac.uk>
2024-12-11 02:31:01 +0800

    chore: adjusted version for mac.

--- main/main.cpp
+++ main/main.cpp
@@ -59,7 +59,7 @@ int main() try
 	//glfwWindowHint( GLFW_RESIZABLE, GLFW_FALSE );
 
 	glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 4 );
-	glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
+	glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 1 );
 	glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE );
 	glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
 
